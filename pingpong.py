add78608dfb7a74d7a3e001dcc72745e844dd846
njordev <weis.winfried@hotmail.com>
2023-04-10 00:36:50 +0200

    Added first paddle

---
+++ pingpong.py
@@ -0,0 +1,24 @@
+# Simple Ping Pong 
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @njordev")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Create the paddles
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Main game loop 
+while True:
+    wn.update()
